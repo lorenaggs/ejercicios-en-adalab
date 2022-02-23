d95046af4e08c48f746d0fd62163d2719f8ffe12
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-02-23 06:55:11 -0500

    Orden de carpetas

--- ejercicios-en-clase/ejercicios-de-los-materiales-main/react-ejercicio-global-twitter/14-props-en-objeto-vs-props-individualmente/src/services/api.js
+++
@@ -1,7 +0,0 @@
-const getTweets = () => {
-  return fetch(
-    'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/twitter-v1/tweets.json'
-  ).then(response => response.json());
-};
-
-export default getTweets;
